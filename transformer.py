3103407fb45e777a318e3854b783c8267209b6c8
Woon Yee Ng <ngnwy289@gmail.com>
2025-03-28 17:04:41 +0800

    feat: feed forward layer

--- transformer.py
+++ transformer.py
@@ -43,6 +43,16 @@ class MultiHeadAttention(nn.Module):
         attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
         output = self.W_o(self.combine_heads(attn_output))
         return output
+    
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff):
+        super(PositionWiseFeedForward, self).__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
 
 
 
