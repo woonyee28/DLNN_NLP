8d13af820491b82788ed9301d2faf9879f2b6cff
Woon Yee Ng <ngnwy289@gmail.com>
2025-03-28 13:03:34 +0800

    feat: update common_utils with bert tokenizer and model for embeddings

--- common_utils.py
+++ common_utils.py
@@ -3,31 +3,51 @@ import os
 import random
 
 from datasets import Dataset
-import nltk
 import torch
 import numpy as np
+from transformers import BertTokenizer, BertModel
 
 SAVE_DIR = "./result/"
 VOCAB_PATH = os.path.join(SAVE_DIR, "vocab.json")
+EMBEDDINGS_PATH = os.path.join(SAVE_DIR, "embeddings.json")
 
 def tokenize(dataset: Dataset, save=False) -> set:
     """
-    Tokenize the text into tokens using nltk
+    Tokenize the text into tokens using Bert Tokenizer
     """
+    tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
     vocab = set()
 
     for example in dataset:
-        tokens = nltk.word_tokenize(example["text"])
+        tokens = tokenizer(example["text"], return_tensors='pt')
         vocab.update(tokens)
 
     print(f"Vocabulary size: {len(vocab)}")
 
     if save:
-        with open(VOCAB_PATH, "w", encoding="etf-8") as f:
+        with open(VOCAB_PATH, "w", encoding="utf-8") as f:
             json.dump(list(vocab), f, ensure_ascii=False, indent=4)
 
         print(f"Vocabulary saved to {VOCAB_PATH}")
     return vocab
+
+def create_embeddings(tokens: set, save=False) -> list:
+    """
+    Convert the tokens to embeddings using Bert Model
+    """
+    model = BertModel.from_pretrained("bert-base-cased")
+    embeddings = []
+
+    for encoded_input in tokens:
+        output_embeddings = model(encoded_input)
+        embeddings.append(output_embeddings)
+    
+    if save:
+        with open(EMBEDDINGS_PATH, "w", encoding="utf-8") as f:
+            json.dump(embeddings, f, ensure_ascii=False, indent=4)
+        
+        print(f"Embeddings saved to {EMBEDDINGS_PATH}")
+    return embeddings
 
 
 def set_seed(seed=0):
