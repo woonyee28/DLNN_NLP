7111998550bd1e173c303db4cb0e0d20f0d4020c
Woon Yee Ng <ngnwy289@gmail.com>
2025-03-28 15:16:10 +0800

    feat: common utils add embedding matrix

--- common_utils.py
+++ common_utils.py
@@ -2,13 +2,15 @@ import json
 import os
 import random
 
-from datasets import Dataset
+from datasets import Dataset, load_dataset
 import nltk
 import torch
 import numpy as np
 
+
 SAVE_DIR = "./result/"
 VOCAB_PATH = os.path.join(SAVE_DIR, "vocab.json")
+EMBEDDING_DIM = 100
 
 def tokenize(dataset: Dataset, save=False) -> set:
     """
@@ -23,7 +25,7 @@ def tokenize(dataset: Dataset, save=False) -> set:
     print(f"Vocabulary size: {len(vocab)}")
 
     if save:
-        with open(VOCAB_PATH, "w", encoding="etf-8") as f:
+        with open(VOCAB_PATH, "w", encoding="utf-8") as f:
             json.dump(list(vocab), f, ensure_ascii=False, indent=4)
 
         print(f"Vocabulary saved to {VOCAB_PATH}")
@@ -39,5 +41,43 @@ def set_seed(seed=0):
     np.random.seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
+
     
+def load_glove_embeddings() -> dict:
+    """
+    Load GloVe embeddings
+    """
+    glove_dict = {}
+    word_embedding_glove = load_dataset("SLU-CSCI4750/glove.6B.100d.txt")
+    word_embedding_glove = word_embedding_glove["train"]
+
+    for example in word_embedding_glove:
+        split_line = example["text"].strip().split()
+        word = split_line[0]
+        vector = np.array(split_line[1:], dtype="float32")
+        glove_dict[word] = vector
+
+    print(f"Total GloVe words loaded: {len(glove_dict)}")
+    return glove_dict
+
+
+def create_embedding_matrix(vocab) -> list:
+    glove_dict = load_glove_embeddings()
+    embedding_matrix = np.zeros((len(vocab), EMBEDDING_DIM))
+
+    missing_words = []
+    for word in vocab:
+        if word not in glove_dict:
+            missing_words.append(word)
+    missing_words_embedding = np.mean(list(glove_dict.values()), axis=0)
+
+    word2idx = {word: idx for idx, word in enumerate(sorted(vocab))}
+    for word, idx in word2idx.items():
+        if word in missing_words:
+            embedding_matrix[idx] = missing_words_embedding
+        else:
+            embedding_matrix[idx] = glove_dict[word]
+
+    return embedding_matrix
+
 
